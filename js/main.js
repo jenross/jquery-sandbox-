7a01ea98290808a0f4b41252f5d8a3b1f4d06d5e
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-07-08 14:20:53 -0400

    got 30 second timer on question to work

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+let qtemplate = '';
+let intervalId;
+let qTimer = 30;
+//will need to add images; might be good to store these in their own js file
 let quiz = [
 {   
     question: 'How many plays did Shakespeare write?',
@@ -12,44 +16,72 @@ let quiz = [
 ];
 
 //need to figure out how to get this to go from question to question
-function handleQAndA() {
-    let qtemplate = `<div class='question-template'>
+function handleQandA() {
+
+    for (let questionCount = 0; questionCount < quiz.length; questionCount++) {
+        
+        qtemplate = `<div class='question-template'>
     
-    <h2>${quiz[0].question}</h2>
+        <h2>${quiz[questionCount].question}</h2>
 
-    <form>
-    <fieldset>
+        <form>
+        <fieldset>
     
-    <label class='answer-choices'>
-    <input type='radio' name='answer-option' value='${quiz[0].answers[0]}'>
-    <span>${quiz[0].answers[0]}</span>
-    </label>
+        <label class='answer-choices'>
+        <input type='radio' name='answer-option' value='${quiz[questionCount].answers[0]}'>
+        <span>${quiz[questionCount].answers[0]}</span>
+        </label>
 
-    <label class='answer-choices'>
-    <input type='radio' name='answer-option' value='${quiz[0].answers[1]}'>
-    <span>${quiz[0].answers[1]}</span>
-    </label>
+        <label class='answer-choices'>
+        <input type='radio' name='answer-option' value='${quiz[questionCount].answers[1]}'>
+        <span>${quiz[questionCount].answers[1]}</span>
+        </label>
 
-    <label class='answer-choices'>
-    <input type='radio' name='answer-option' value='${quiz[0].answers[2]}'>
-    <span>${quiz[0].answers[2]}</span>
-    </label>
+        <label class='answer-choices'>
+        <input type='radio' name='answer-option' value='${quiz[questionCount].answers[2]}'>
+        <span>${quiz[questionCount].answers[2]}</span>
+        </label>
 
-    <label class='answer-choices'>
-    <input type='radio' name='answer-option' value='${quiz[0].answers[3]}'>
-    <span>${quiz[0].answers[3]}</span>
-    </label>
+        <label class='answer-choices'>
+        <input type='radio' name='answer-option' value='${quiz[questionCount].answers[3]}'>
+        <span>${quiz[questionCount].answers[3]}</span>
+        </label>
     
-    </fieldset>
-    </form>
-    </div>`;
-
+        </fieldset>
+        </form>
+        </div>`;
+        
+    }
+    // questionCount++;
     $('.qanda-disp').append(qtemplate);
 }
 
-handleQAndA();
+handleQandA();
+questionTimerRun();
+
+function questionTimerRun() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+}
+
+function decrement() {
+    qTimer--;
+    $('.timer').text(qTimer);
+    if (qTimer === 0) {
+        clearInterval(intervalId);
+        //handleResponse();
+    }
+}
 
 function handleResponse()  {
 
 }
 
+function nextQuestion() {
+    $('.qanda-disp').empty();
+
+}
+
+function startQuiz() {
+
+}
